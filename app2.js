484af893f8cd552057bc7691ce19f46e1450c090
quannguyen <waynenguyenus@gmail.com>
2022-07-31 15:26:44 -0700

    1

--- app2.js
+++ app2.js
@@ -34,46 +34,47 @@ document.getElementById("inputValue").addEventListener("input", function(e) {
     console.log(y1);
     console.log(y3);
 
-    var x2 = document.getElementById("select2").selectedIndex;
-    var y2 = document.getElementsByTagName("option")[x2].value
-    var x4 = document.getElementById("select4").selectedIndex;
-    var y4 = document.getElementsByTagName("option")[x4].value
-    console.log(y2);
-    console.log(y4);
-    // if (y1 !== y2){
-    //   if (y1 == "Lbs" && y2 == "Kg") {
-    //     let lbs = e.target.value;
-    //     document.getElementById("result").innerHTML = lbs / 2.2046;
-        
-    //   }
-    //   else if (y1 == "Kg" && y2 == "Lbs") {
-    //     let kg = e.target.value;
-    //     document.getElementById("result").innerHTML = kg * 2.2046;
-        
-    //   }
-    //   else if (y1 == "Km" && y2 == "Miles") {
-    //     let km = e.target.value;
-    //     document.getElementById("result").innerHTML = km / 1.609;
-        
-    //   }
-    //   else if (y1 == "F" && y2 == "C") {
-    //     let f = e.target.value;
-    //     document.getElementById("result").innerHTML = (f - 32) * 5/9;
-        
-    //   }
-    //   else if (y1 == "C" && y2 == "F") {
-    //     let c = e.target.value;
-    //     document.getElementById("result").innerHTML = (c * 9/5) + 32 ;
-        
-    //   }
+    var x2 = document.getElementById("select2").childNodes[0].innerHTML
+    var x2b = document.getElementById("select2").childNodes[1].innerHTML
+    var x4 = document.getElementById("select4").childNodes[0].innerHTML
+    var x4b = document.getElementById("select4").childNodes[1].innerHTML
+
+
     
-    //   else {
-    //     document.getElementById("result").innerHTML = "Units are difference and not convertable";
-    //   }
-    // }
-    // else{
-    //   document.getElementById("result").innerHTML = "Same Unit: Error";
-    // }
+    if (y1 !== y2){
+      if (y1 == "Lbs" && y2 == "Kg") {
+        let lbs = e.target.value;
+        document.getElementById("result").innerHTML = lbs / 2.2046;
+        
+      }
+      else if (y1 == "Kg" && y2 == "Lbs") {
+        let kg = e.target.value;
+        document.getElementById("result").innerHTML = kg * 2.2046;
+        
+      }
+      else if (y1 == "Km" && y2 == "Miles") {
+        let km = e.target.value;
+        document.getElementById("result").innerHTML = km / 1.609;
+        
+      }
+      else if (y1 == "F" && y2 == "C") {
+        let f = e.target.value;
+        document.getElementById("result").innerHTML = (f - 32) * 5/9;
+        
+      }
+      else if (y1 == "C" && y2 == "F") {
+        let c = e.target.value;
+        document.getElementById("result").innerHTML = (c * 9/5) + 32 ;
+        
+      }
+    
+      else {
+        document.getElementById("result").innerHTML = "Units are difference and not convertable";
+      }
+    }
+    else{
+      document.getElementById("result").innerHTML = "Same Unit: Error";
+    }
   
   
   
@@ -81,4 +82,20 @@ document.getElementById("inputValue").addEventListener("input", function(e) {
   
   });
   
+  document.getElementById("inputValue").addEventListener("input", function(e) {
+var x2 = document.getElementById("select2").childNodes[0].innerHTML
+var x2b = document.getElementById("select2").childNodes[1].innerHTML
+var x4 = document.getElementById("select4").childNodes[0].innerHTML
+var x4b = document.getElementById("select4").childNodes[1].innerHTML
+// console.log(x2);
+// console.log(x2b);
+// console.log(x4);
+// console.log(x4b);
+    if (x2 !== x4b) {
+      console.log(true);
+    }
+    else if (x2 == x4b) {
+      console.log(false);
+    }
 
+  })
