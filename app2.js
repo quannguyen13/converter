377fafae14d71fc644f269a6bfed7bb2d97fb569
quannguyen <waynenguyenus@gmail.com>
2022-08-02 21:50:18 -0700

    1

--- app2.js
+++ app2.js
@@ -31,71 +31,74 @@ document.getElementById("inputValue").addEventListener("input", function(e) {
     var y1 = document.getElementsByTagName("option")[x1].value
     var x3 = document.getElementById("select3").selectedIndex;
     var y3 = document.getElementsByTagName("option")[x3].value
-    console.log(y1);
-    console.log(y3);
-
-    var x2 = document.getElementById("select2").childNodes[0].innerHTML
-    var x2b = document.getElementById("select2").childNodes[1].innerHTML
-    var x4 = document.getElementById("select4").childNodes[0].innerHTML
-    var x4b = document.getElementById("select4").childNodes[1].innerHTML
+    // console.log(y1);
+    // console.log(y3);
 
 
-    
-    if (y1 !== y2){
-      if (y1 == "Lbs" && y2 == "Kg") {
-        let lbs = e.target.value;
-        document.getElementById("result").innerHTML = lbs / 2.2046;
-        
-      }
-      else if (y1 == "Kg" && y2 == "Lbs") {
-        let kg = e.target.value;
-        document.getElementById("result").innerHTML = kg * 2.2046;
-        
-      }
-      else if (y1 == "Km" && y2 == "Miles") {
-        let km = e.target.value;
-        document.getElementById("result").innerHTML = km / 1.609;
-        
-      }
-      else if (y1 == "F" && y2 == "C") {
-        let f = e.target.value;
-        document.getElementById("result").innerHTML = (f - 32) * 5/9;
-        
-      }
-      else if (y1 == "C" && y2 == "F") {
-        let c = e.target.value;
-        document.getElementById("result").innerHTML = (c * 9/5) + 32 ;
-        
-      }
-    
-      else {
-        document.getElementById("result").innerHTML = "Units are difference and not convertable";
-      }
-    }
-    else{
-      document.getElementById("result").innerHTML = "Same Unit: Error";
-    }
-  
-  
-  
-  
-  
-  });
-  
-  document.getElementById("inputValue").addEventListener("input", function(e) {
-var x2 = document.getElementById("select2").childNodes[0].innerHTML
-var x2b = document.getElementById("select2").childNodes[1].innerHTML
-var x4 = document.getElementById("select4").childNodes[0].innerHTML
-var x4b = document.getElementById("select4").childNodes[1].innerHTML
-// console.log(x2);
-// console.log(x2b);
-// console.log(x4);
-// console.log(x4b);
-    if (x2 !== x4b) {
-      console.log(true);
-    }
-    else if (x2 == x4b) {
-      console.log(false);
-    }
+    var x2b = document.getElementById("select2").selectedIndex
+    var x4b = document.getElementById("select4").selectedIndex
+    console.dir(x2b);
+    console.log(x4b);
 
-  })
+if (y1 == "weight" && y3 == "weight") {
+    if ( x2b == 1 && x4b == 0) {
+      let lbs = e.target.value;
+      document.getElementById("result").innerHTML = lbs / 2.2046; 
+  }
+  
+    else if ( x2b == 0 && x4b == 1 ) {
+      let kg = e.target.value;
+      document.getElementById("result").innerHTML = kg * 2.2046;  
+  }
+    else if ( x2b == x4b ){
+      document.getElementById("result").innerHTML = "Units are SAME and not convertable";
+  }
+}
+
+
+
+
+
+else if (y1 == "distance" && y3 == "distance"){
+    if ( x2b == 0 && x4b == 1) {
+      let miles = e.target.value;
+      document.getElementById("result").innerHTML = miles * 1.609;
+
+  }
+    else if ( x2b == 1 && x4b == 0) {
+      let km = e.target.value;
+      document.getElementById("result").innerHTML = km / 1.609;
+      
+  }
+    else if ( x2b == x4b ){
+      document.getElementById("result").innerHTML = "Units are SAME and not convertable";
+  }
+}
+
+
+
+
+else if (y1 == "temp" && y3 == "temp"){
+    if ( x2b == 0 && x4b == 1) {
+      let f = e.target.value;
+      document.getElementById("result").innerHTML = (f - 32) * 5/9;
+
+  }
+    else if ( x2b == 1 && x4b == 0) {
+      let c = e.target.value;
+      document.getElementById("result").innerHTML = (c * 9/5) + 32 ;
+      
+  }
+    else if ( x2b == x4b ){
+      document.getElementById("result").innerHTML = "Units are SAME and not convertable";
+  }
+}
+
+
+else if ( y1 !== y3 ) {
+      document.getElementById("result").innerHTML = "Catergories are difference and NOT convertable";
+}
+
+
+}
+)
